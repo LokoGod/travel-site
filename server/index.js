a178257bcd9d88f04a9381ea75d9786a47a9b210
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-09-09 20:20:42 +0530

    latest

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
-import { express } from "express";
-import nodemon from "nodemon";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const express = express();
+// Configs
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// Routes
 
+// SQL setup
